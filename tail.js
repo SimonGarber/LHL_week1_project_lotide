858676fc125c5cc101e0e08e0034483487de591e
simongarber <simon.garber@gmail.com>
2019-07-01 16:38:22 +0000

    modified the tail function. refactored to remove the test code and placed it in the test file.

--- tail.js
+++ tail.js
@@ -1,15 +1,10 @@
-const assertEqual = function(actual, expected) {
 
-    if (actual === expected) {
-        console.log(`✅ ✅ ✅ Assertion Passed:  ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-        console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
-    }
-};
-
+const assertEqual = require('./assertEqual')
 const tail = function(arr){
     return arr.slice(1);
     
 }
-assertEqual(tail([1,2,3]),[2,3]);
+
+module.exports = tail;
+
 
