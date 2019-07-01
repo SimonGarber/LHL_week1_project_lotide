999c6f48c4fcf64eccdf27aeb6ff0143d88f8a30
simongarber <simon.garber@gmail.com>
2019-07-01 17:08:56 +0000

    Modified the code to remove the test cases and place them in the newly created eqArraysTest file. After utilizing require and export, the function is now modular throughout the code.

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
@@ -18,10 +12,7 @@ const eqArrays = function(arr1,arr2) {
   return true;
 };
 
+module.exports = eqArrays;
 
-assertEqual(eqArrays(["1","2","3"],["1","2","3"]), true);
-assertEqual(eqArrays(["1","2","3"],["1","2",3]), false);
-assertEqual(eqArrays([1,2,3],[1,2,3]),true);
-assertEqual(eqArrays([1,2,3],[3,2,1]),false);
   
   
